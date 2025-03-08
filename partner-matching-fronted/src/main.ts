a80e119b1a3e6b78584a1037b53d382db8a471a5
Jaylan <1419349524@qq.com>
2025-03-08 20:59:27 +0800

    add:引入Vant中的Button组件

--- partner-matching-fronted/src/main.ts
+++ partner-matching-fronted/src/main.ts
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { Button } from 'vant'; // 引入一个 vant中的按钮组件
+import 'vant/lib/index.css'; // 引入vant中的界面样式
 
 // 创建一个APP 挂在上一个app组件
 // #app组件是 id 选择器 选择的index.html中(墙)
-createApp(App).mount('#app')
+
+const app = createApp(App); // 创建一个app对象
+app.use(Button); //app对象使用引入的Button按钮
+app.mount('#app') // 将app挂在到墙(#app)Id选择器上
+
+
