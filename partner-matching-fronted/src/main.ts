f5223142d877db5547c68d3b6ef3c59f22ccc904
Jaylan <1419349524@qq.com>
2025-03-08 20:52:38 +0800

    other:新增注释

--- partner-matching-fronted/src/main.ts
+++ partner-matching-fronted/src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// 创建一个APP 挂在上一个app组件
+// #app组件是 id 选择器 选择的index.html中(墙)
 createApp(App).mount('#app')
--- partner-matching-fronted/vite.config.ts
+++ partner-matching-fronted/vite.config.ts
@@ -2,7 +2,10 @@ import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from '@vant/auto-import-resolver';
+// 从依赖中引入插件
 
+// 插件中包含 vue 还有 AutoImport 以及Components
+// VantResolver() 面向Vant的解决方案
 export default {
   plugins: [
     vue(),
