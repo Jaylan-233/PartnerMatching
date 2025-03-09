f638d1a9bb1f3227f59becce2f99253efba969a5
Jaylan <1419349524@qq.com>
2025-03-09 09:48:15 +0800

    feat:增加基础布局

---
+++ partner-matching-fronted/src/layouts/BasicLayout.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <van-nav-bar
+      title="标题"
+      left-text="返回"
+      right-text="按钮"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+  />
+
+</template>
+
+<script  setup>
+import { showToast } from 'vant';
+const onClickLeft = () => alert("左边");
+const onClickRight = () => alert("右边")
+
+
+</script>
+
+
+
+<style scoped>
+
+</style>
